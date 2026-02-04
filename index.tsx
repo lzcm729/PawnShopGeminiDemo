96d261c0015c3c0158fdf56e088921a7a94a148a
lzcm729 <lzcm.729@gmail.com>
2026-02-04 23:31:59 +0800

    Revert "refactor(build): Migrate Tailwind CSS from CDN to PostCSS"
    This reverts commit db891e21ebd49aa4796dc06158f212a34f8cdb49.

--- index.tsx
+++ index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import { loadItemDataFromFiles } from './systems/items/dataInit';
 
